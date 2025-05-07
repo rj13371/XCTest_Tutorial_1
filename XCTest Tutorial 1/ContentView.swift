8a402b6ef9686a751003cb8f7bd59716491b28df
Roland <rj1337@gmail.com>
2025-05-07 17:46:27 +0900

    Initial Commit

---
+++ XCTest Tutorial 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XCTest Tutorial 1
+//
+//  Created by roland on 2025/05/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XCTest Tutorial 1/XCTest_Tutorial_1App.swift
@@ -0,0 +1,17 @@
+//
+//  XCTest_Tutorial_1App.swift
+//  XCTest Tutorial 1
+//
+//  Created by roland on 2025/05/07.
+//
+
+import SwiftUI
+
+@main
+struct XCTest_Tutorial_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
